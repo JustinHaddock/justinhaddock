b30f606f9ca5d40a59c9b455fdf1b074623980b9
Justin Haddock <jch4411@rit.edu>
2015-09-09 20:40:52 -0500

    First push

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var uncss = require('gulp-uncss');
+var concatCss = require('gulp-concat-css');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+
+gulp.task('default', ['compress'], function() {
+  return gulp.src('css/**/*.css')
+    .pipe(concatCss("css/bundle.css"))
+    .pipe(uncss({
+        html: ['**/*.html']
+    }))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(minifyCss({compatibility: 'ie8',keepSpecialComments: 0,keepBreaks: true}))
+    .pipe(gulp.dest('out/'));
+});
+
+gulp.task('compress', function() {
+  return gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('out/'));
+});
