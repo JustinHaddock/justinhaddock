794b2a303bf4c2039e17eafde9629244a7c2b996
Justin <justin@cremalab.com>
2015-09-10 09:33:48 -0500

    Pushing from mac

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ myApp.config(['$routeProvider', function($routeProvider) {
 	when('/1-2', {
 		templateUrl: 'partials/1/1-2.html'
 	}).
-	when('/-3', {
+	when('/1-3', {
 		templateUrl: 'partials/1/1-3.html'
 	}).
 	when('/1-4', {
