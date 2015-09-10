344d93ed562abad2911daa3f4b717e376c12fa8d
Justin Haddock <justinhaddock2013@yahoo.com>
2015-09-09 23:05:42 -0500

    comic book routing is done

--- js/app.js
+++ js/app.js
@@ -11,6 +11,18 @@ myApp.config(['$routeProvider', function($routeProvider) {
 	when('/1', {
 		templateUrl: 'partials/1/1.html'
 	}).
+	when('/1-1', {
+		templateUrl: 'partials/1/1-1.html'
+	}).
+	when('/1-2', {
+		templateUrl: 'partials/1/1-2.html'
+	}).
+	when('/-3', {
+		templateUrl: 'partials/1/1-3.html'
+	}).
+	when('/1-4', {
+		templateUrl: 'partials/1/1-4.html'
+	}).
 	when('/2', {
 		templateUrl: 'partials/2/2.html'
 	}).
