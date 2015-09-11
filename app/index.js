32388fc4b261539f3465e1e2e244673fa4666411
Justin <justin@cremalab.com>
2015-09-11 14:17:33 -0500

    pushing what works locally AND on gh-pages

--- app/index.js
+++ app/index.js
@@ -33066,27 +33066,27 @@ angular.module("mySite").config(['$urlRouterProvider','$stateProvider', function
     .state('home', {
       url: "/home",
       controller: "l1Controller as cont",
-      templateUrl: "/partials/l0.html"
+      templateUrl: "/justinhaddock/partials/l0.html"
     })
     .state('l2', {
       url: "/:pNum",
       controller: "l2Controller as cont",
       resolve: {
         promiseObj:  function($http){
-          return $http({method: 'GET', url: '/assets/l2data.json'});
+          return $http({method: 'GET', url: '/justinhaddock/assets/l2data.json'});
         }
       },
-      templateUrl: "/partials/l1.html"
+      templateUrl: "/justinhaddock/partials/l1.html"
     })
       .state('l3', {
       url: "/:pNum/:cNum",
       controller: "l3Controller as cont",
       resolve: {
         promiseObj:  function($http){
-          return $http({method: 'GET', url: '/assets/l3data.json'});
+          return $http({method: 'GET', url: '/justinhaddock/assets/l3data.json'});
         }
       },
-      templateUrl: "/partials/l2.html"
+      templateUrl: "/justinhaddock/partials/l2.html"
     })
 }]);
 
